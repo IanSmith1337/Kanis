324315ef559b2f7e2c216e6471360a387ab33da6
iansmith1337 <iansmith1337@gmail.com>
2021-10-26 14:58:22 -0400

    Updates

--- Kanis/README.md
+++ Kanis/README.md
@@ -1,1 +1,4 @@
-# Kanis
+# Kanis, the minecraft palamute/palico mod.
+
+## WIP
+
--- Kanis/src/main/java/com/ismith/kanismod/ModManager.java
+++ Kanis/src/main/java/com/ismith/kanismod/ModManager.java
@@ -14,7 +14,7 @@ public class ModManager implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final Logger LOGGER = LogManager.getLogger("kanis");
 
 	public static final Item Kanis_Armor = new Item(new FabricItemSettings().group(ItemGroup.MISC));
 
