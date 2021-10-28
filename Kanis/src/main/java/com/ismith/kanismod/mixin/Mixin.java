9d1e79b14b00838455f68f2f7cda74e0a403aeee
iansmith1337 <iansmith1337@gmail.com>
2021-10-28 18:57:43 -0400

    Fun with mixins

--- Kanis/src/main/java/com/ismith/kanismod/mixin/Mixin.java
+++ Kanis/src/main/java/com/ismith/kanismod/mixin/Mixin.java
@@ -1,14 +1,21 @@
 package com.ismith.kanismod.mixin;
 
 import net.minecraft.entity.passive.WolfEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Hand;
+
+import com.ismith.kanismod.ModManager;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @org.spongepowered.asm.mixin.Mixin(WolfEntity.class)
 public class Mixin {
-	@Inject(method = "interactMob(Lnet.minecraft.entity.player.PlayerEntityLnet.minecraft.util.Hand)Lnet/minecraft/util/ActionResult", at = @At("TAIL"))
-	private void func(CallbackInfo info) {
-		System.out.println("Successfully called mixin.");
+	@Inject(at = @At("HEAD"), method = "interactMob", cancellable = true)
+	private void test(PlayerEntity p, Hand h, CallbackInfoReturnable<Object> returnable) {
+		WolfEntity currentEntity = (WolfEntity) (Object) this;
+		if (currentEntity.isTamed()) {
+			ModManager.LOGGER.info("This is " + currentEntity.getEntityName() + " owned by " +  currentEntity.getOwner());
+		}
 	}
 }
